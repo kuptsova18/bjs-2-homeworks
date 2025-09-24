928363090afb332654f8ba870389fd9eac783470
eekatzee18 <kuptsova.98@list.ru>
2025-09-24 09:15:23 +0400

    Оба задания полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,14 +3,13 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = Math.pow(b,2)-4*a*c;
-	console.log(d);
 	if (d < 0) {
 		return arr;
 	} else if (d === 0) {
 		let x = -b / (2 * a);
 		arr.push(x);
 		return arr;
-	} else if (d > 0) {
+	} else {
 		let x1 = (-b + Math.sqrt(d)) / (2 * a);
 		let x2 = (-b - Math.sqrt(d)) / (2 * a);
 		arr.push(x1, x2);
