0d751cdf00b7ca9317a78902f212a473da3f355b
eekatzee18 <kuptsova.98@list.ru>
2025-10-13 13:11:15 +0400

    задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,47 @@
-﻿
+﻿function parseCount(value) {
+    let parseValue = Number.parseFloat(value);
+    if (isNaN(parseValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parseValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    }
+    catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        let s = Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c));
+        return Number(s.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a,b,c);
+    }
+    catch (error) {
+        
+    }
+}
